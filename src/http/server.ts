931ef264b1e1844bba9c8e3a8c894f17c9a998d6
Thiago Elias <thiagoelias99@gmail.com>
2024-09-11 13:15:43 -0300

    feat: :sparkles: Create goal route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,30 @@
 import fastify from "fastify"
+import { createGoal } from '../features/create-goal'
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post('/goals', {
+  schema: {
+    body: z.object({
+      title: z.string(),
+      desiredWeeklyFrequency: z.number().min(1).max(7)
+    })
+  }
+}
+  , async (req) => {
+
+    const { title, desiredWeeklyFrequency } = req.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency
+    })
+  })
 
 app.listen({
   port: 3333
